12c81261b64025753a32ffe6c1d63b478fc0517a
JulioGaspareto <juliogaspareto0@gmail.com>
2025-04-15 19:54:01 -0300

    adicionei Aula HTML

---
+++ Atividades Js/ResoluçãoListaDeExercicíos1_profAlex/exercicio8.js
@@ -0,0 +1,11 @@
+let combustivel = prompt("Quanto de combustivel tem ?")
+let climaPlaneta = prompt("o clima esta bom (sim/nao)")
+
+// climaPlaneta = climaPlaneta.toLocaleLowerCase
+
+
+if (combustivel >= 50 && climaPlaneta == "sim"){
+    console.log("Pouso autorizado!")
+}else{
+    console.log("Abortar Missão !")
+}
