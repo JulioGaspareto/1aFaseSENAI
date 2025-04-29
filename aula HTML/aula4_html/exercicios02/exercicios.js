5db28037bd715937ee81e32b5d5418f075ec005a
JulioGaspareto <julio_gaspareto@estudante.sesisenai.org.br>
2025-04-28 22:04:03 -0300

    Finalizando Lista de exercicios 02 Prof Alex

--- aula HTML/aula4_html/exercicios02/exercicios.js
+++ aula HTML/aula4_html/exercicios02/exercicios.js
@@ -1,3 +1,5 @@
+ let contador = 0
+
 function Exercicio3(){
 
     let nomeUser = prompt("Digite o seu nome e receba seu poder aleatorio")
@@ -36,4 +38,52 @@ function Exercicio3(){
     } else if (poder == 10){
         alert(`viking ${nomeUser}, Força de mil homens`)
     }
+}
+function Exercicio4(){
+    
+    let signoUser = prompt("Digite o seu signo:")
+
+    signoUser = signoUser.toLowerCase() 
+
+    // Áries, Touro, Gêmeos, Câncer, Leão, Virgem, Libra, Escorpião, Sagitário, Capricórnio, Aquário e Peixes
+
+    aleatorio = Math.round(Math.random()*(3-1)+1)
+    if(aleatorio == 3 ){
+        alert(`Hoje seu signo de ${signoUser},está propicio a morte`)
+    }else if (aleatorio == 2){
+        alert(`o signo de ${signoUser} ira encontrar seu amor verdadeiro`)
+
+    }else {
+        alert (`o signo de ${signoUser} está com sorte hoje`)
+    }
+
+}
+
+function Exercicio10(){
+
+    let numberUser = Number(prompt("Digite um numero de 0 à 10 "))
+
+    let ParOuImpar = Number(prompt("1 - Impar \n 2 - Par \n Digite o valor Para saber se é par ou impar"))
+
+    sistemaNumero = Math.round(Math.random()*(10-0)+0)
+
+    valorTotal = (sistemaNumero + numberUser)%2
+
+    if (valorTotal == 0 && ParOuImpar == 1) {
+        alert ("O resultado foi Par, e voce escolheu Impar \n Entao voce perdeu")
+
+    }else if (valorTotal == 0 && ParOuImpar == 2 ) {
+        alert ("O resultado foi Par, e voce escolhei Par \n Entao voce ganhou")
+    }else if (valorTotal == 1 && ParOuImpar == 1 ){
+        alert("O resultado foi Impar, e voce escolheu Impar \n Entao voce ganhou")
+    } else { 
+        alert ("o resultado foi Impar, e voce escolheu Par \n Entao voce perdeu")
+    }
+}
+
+function Exercicio14(){
+    contador++
+}
+function Resultado(){
+    alert("voce clicou "+ contador)
 }
