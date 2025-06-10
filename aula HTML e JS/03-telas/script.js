26e27c7196f3656321038322a95f2ebf54653a9b
JulioGaspareto <juliogaspareto0@gmail.com>
2025-06-09 22:06:18 -0300

    usuario array

--- aula HTML e JS/03-telas/script.js
+++ aula HTML e JS/03-telas/script.js
@@ -1,26 +1,37 @@
-let usuario = {
-    nome: "um",
-    email: "dois",
-    senha:  "tres"
-    
-}
+inicializar()
+
+let usuarios = []
+
 function registrarUser(){
-    usuario.nome = document.getElementById("nome").value
-    usuario.email = document.getElementById("emailRegister").value
-    usuario.senha = document.getElementById("senhaRegister").value
+
+    let usuario = {
+        
+    nome:  document.getElementById("nome").value,
+    email: document.getElementById("emailRegister").value,
+    senha: document.getElementById("senhaRegister").value
+    }
+    usuarios.push(usuario)
+
     alert("cadastrado!!!!")
-    console.log(usuario)
+
+    console.log(usuarios)
+    mostrarLogin()
+    
 }
 function logarUser(){
    let email = document.getElementById("emailLogar").value
     let senha = document.getElementById("senhaLogar").value
-    
-    if(email === usuario.email && senha === usuario.senha){
-        alert("login efetuado com sucesso!")
-        mostrarProdutos()
-    }else{
-        alert("Login nao deu ai ")
+
+    for(let i = 0 ; i<usuarios.length; i++){
+        
+        if(email === usuarios[i].email && senha === usuarios[i].senha){
+            alert("login efetuado com sucesso!\n Seja bem vindo " + usuarios[i].nome)
+            mostrarProdutos()
+       }//else{
+        //     alert("Login nao deu ai ")
+        // }
     }
+    
 }
 function mostrarLogin(){
     esconderTodas()
@@ -40,8 +51,9 @@ function mostrarProdutos(){
     esconderTodas()
     mostrarBotao()
     document.getElementById("produtos").style.display = "flex"
+    document.getElementById("navBar").style.display = "flex"
      document.getElementById("esconderBP").style.display = "none"
-
+  
 }
 function esconderTodas(){
     document.getElementById("login").style.display = "none"
@@ -54,4 +66,8 @@ function mostrarBotao(){
     document.getElementById("esconderBL").style.display = "flex"
      document.getElementById("esconderBC").style.display = "flex"
       document.getElementById("esconderBP").style.display = "flex"
+}
+
+function inicializar(){
+    mostrarLogin
 }
